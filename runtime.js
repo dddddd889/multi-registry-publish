0f82e3f2ae0f55f41cab8f29f50801717e472fe2
yhding <yhding9527@hotmail.com>
2024-11-21 17:13:35 +0800

    chore: 处理引用问题

--- runtime.js
+++ runtime.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const { jsonReader } = require('./lib/utils/jsonOp')
 
-const { data: cliPkg } = jsonReader(path.resolve(__dirname, PKG_NAME))
-
-exports.cliName = cliPkg?.name
 exports.PKG_NAME = 'package.json'
+
+const { data: cliPkg } = jsonReader(path.resolve(__dirname, PKG_NAME))
+exports.cliName = cliPkg?.name
 exports.CACHE_NAME = `.${cliPkg?.name}`
