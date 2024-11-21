4447ff828da88a5624f1b1b67fea6c66a22ece1f
yhding <yhding9527@hotmail.com>
2024-11-21 14:19:14 +0800

    chore: 处理脚本找到宿主环境

--- scripts/addPkgPublishHooks.js
+++ scripts/addPkgPublishHooks.js
@@ -1,18 +1,28 @@
 const { PKG_NAME } = require('../lib/constants/var')
 const { jsonReader, jsonWriter } = require('../lib/utils/jsonOp')
-const { data: pkg } = jsonReader(PKG_NAME)
+const path = require('path')
+const logger = require('../lib/utils/logger')
 
 function suffixCli(postpublish) {
 	return `${postpublish ? `${postpublish} && ` : ``}multi-registry-publish`
 }
 
 function addPkgPublishHooks() {
+	const hostPath = process.env.INIT_CWD
+	if (!hostPath) {
+		logger.error(`请手动添加指令到项目环境中
+"prepublishOnly": "multi-registry-publish",
+"postpublish": "multi-registry-publish"`)
+		return
+	}
+	const pkgPath = path.resolve(hostPath, PKG_NAME)
+	const { data: pkg } = jsonReader(pkgPath)
 	if (!pkg?.scripts) {
 		pkg.scripts = {}
 	}
 
 	pkg.scripts.prepublishOnly = suffixCli(pkg?.scripts?.prepublishOnly)
 	pkg.scripts.postpublish = suffixCli(pkg?.scripts?.postpublish)
-	jsonWriter(PKG_NAME, pkg)
+	jsonWriter(pkgPath, pkg)
 }
 addPkgPublishHooks()
